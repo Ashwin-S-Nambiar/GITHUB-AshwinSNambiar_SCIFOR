97393e7a84435ffe1598f9b4b7e03d54179543cf
Ashwin S Nambiar <76719333+Ashwin-S-Nambiar@users.noreply.github.com>
2024-09-27 19:12:33 +0530

    Test-11 files added.

---
+++ Tests/Test-11/react-class-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Car extends React.Component {
+  render() {
+    return( 
+            <div class="container">
+                <h2>This from Class Component</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores iure quaerat atque amet expedita vitae similique deserunt nostrum. Similique fugit perferendis commodi aut aspernatur dolor eum optio aliquam doloribus soluta.</p>
+            </div>
+     )
+  }
+}
+
+export default Car
---
+++ Tests/Test-11/react-function-app/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function App() {
+    return( 
+        <div class="container">
+            <h2>This is from Funtional Component</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores iure quaerat atque amet expedita vitae similique deserunt nostrum. Similique fugit perferendis commodi aut aspernatur dolor eum optio aliquam doloribus soluta.</p>
+        </div>
+ )
+}
